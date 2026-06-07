0c2167e52c358b4b2c5fbb8778af1af59ef5d6c1
Sudeep Cholkar <sudeepcholkar918@gmail.com>
2026-06-08 00:29:40 +0530

    subnet added

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,26 @@ output "vpc_id" {
     value = aws_vpc.main.id
 }
 
-output "az_info" {
-    value = data.aws_availability_zones.available
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
 }
 
-output "default_vpc_info" {
-    value = data.aws_vpc.default
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
 }
 
-output "main_route_table_info" {
-    value = data.aws_route_table.main
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
 }
+
+# output "az_info" {
+#     value = data.aws_availability_zones.available
+# }
+
+# output "default_vpc_info" {
+#     value = data.aws_vpc.default
+# }
+
+# output "main_route_table_info" {
+#     value = data.aws_route_table.main
+# }
